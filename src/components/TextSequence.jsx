0a0edf6064880929703c88c94fc6e0581c6f9f3d
donix15k <165041166+donix15k@users.noreply.github.com>
2026-02-10 17:17:51 -0400

    Prepare for GitHub Pages deploy

--- src/components/TextSequence.jsx
+++ src/components/TextSequence.jsx
@@ -1,7 +1,14 @@
-function TextSequence({ text }) {
+function TextSequence({ text, color }) {
   return (
     <section className="overlay bottom-layer">
-      <p key={text} className="sequence-text fade-in-out">
+      <p
+        key={text}
+        className="sequence-text fade-in-out"
+        style={{
+          color: color || '#ffe2cb',
+          textShadow: `0 0 26px ${color || 'rgba(255, 128, 82, 0.45)'}`,
+        }}
+      >
         {text}
       </p>
     </section>
